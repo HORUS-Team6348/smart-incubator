1ccf9dad6716f01e12bd0f0a9db08f0ae56d93fd
Pablo Ortega <2001.paok@gmail.com>
2018-05-05 18:21:57 -0500

    Sensor init. Temperature read.

--- Classes.py
+++ Classes.py
@@ -1,10 +1,40 @@
-class Sensor_init:
-    def __init__(self, pin):
+class Sensor:
+
+    def config(self, pin, resolution):
         import machine
         import onewire
         self.dat = machine.Pin(pin)
         self.ds  = onewire.DS18B20(onewire.OneWire(self.dat))
-        return self.ds
+
+        self.ds.reset()
+        self.select()
+        self.ds.write(0x4E)
+        self.ds.write(0x00)
+        self.ds.write(0x00)
+        if resolution == "9":
+            self.ds.write(0x1f)
+        if resolution == "10":
+            self.ds.write(0x3f)
+        if resolution =="11":
+            self.ds.write(0x5f)
+
+
+
+
+
+
+    def init_read(self, ds):
+        ds.convert_temp()
+
+    def read (self, ds, rom):
+        ds.read_temp(rom)
+
+
+
+
+
+
+
 
 
 
