e2faaaf85432dc30d9ad78732e4378ebdac0737d
Emilio Mendoza <emilio2601@gmail.com>
2018-05-05 22:21:35 -0500

    Madres

--- boot.py
+++ boot.py
@@ -1,4 +1,5 @@
 import esp
+import utime
 import picoweb
 import machine
 import network
@@ -6,30 +7,49 @@ from temperature import DS18B20x
 
 ESSID   = "Incubator-1"
 
-incubator_temperature = DS18B20x(12)
-peltier_temperature   = DS18B20x(13)
+peltier_fast    = DS18B20x(12, 11)
+peltier_exact   = DS18B20x(13, 12)
+incubator_fast  = DS18B20x(14, 11)
+incubator_exact = DS18B20x(15, 12)
 
-timer = machine.Timer(0)
-
-counter = 0
+fast_timer  = machine.Timer(0)
+exact_timer = machine.Timer(1)
 
 
 def nodebug():
     esp.osdebug(None)
 
+def fast_timer_IRQ(timer):
+    pass
 
-def timerIRQ():
-    global counter
-    counter += 1
-    print("IRQ here!")
-    print(counter)
+def exact_timer_IRQ(timer):
+    pass
 
 
 def init():
+    print("Initializing WiFi AP...")
     ap = network.WLAN(network.AP_IF)
-    ap.active(True) 
+    ap.active(True)
+    print("WiFi AP initialized")
 
-    timer.init(period=380, mode=machine.Timer.PERIODIC, callback=timerIRQ)
+    print("Initializing temperature sensors...")
+    """peltier_fast.init_read()
+    peltier_exact.init_read()
+    incubator_fast.init_read()
+    incubator_exact.init_read()"""
+    print("Temperature sensors initialized")
 
+    print("Waiting for first readings...")
+    utime.sleep_ms(750)
+    print("Wait over.")
+
+    print("Initializing control loop timers...")
+    fast_timer.init(period=380, mode=machine.Timer.PERIODIC, callback=fast_timer_IRQ)
+    exact_timer.init(period=755, mode=machine.Timer.PERIODIC, callback=exact_timer_IRQ)
+    print("Control loop timers initialized")
 
 init()
+
+
+while True:
+    pass
--- temperature.py
+++ temperature.py
@@ -1,3 +1,70 @@
+import machine
+import onewire
+
+_CONVERT = const(0x44)
+_RD_SCRATCH = const(0xbe)
+_WR_SCRATCH = const(0x4e)
+
 class DS18B20x:
-    def __init__(self):
-        pass
+    def __init__(self, pin, resolution):
+       self.pin = machine.Pin(pin)
+       self.ow  = onewire.OneWire(self.pin)
+
+        if resolution == 12:
+            return
+
+        self.ds.reset()
+        self.ds.select()
+        self.ds.write(0x4E)
+        self.ds.write(0x00)
+        self.ds.write(0x00)
+        if resolution == 9:
+            self.ds.write(0x1f)
+        if resolution == 10:
+            self.ds.write(0x3f)
+        if resolution == 11:
+            self.ds.write(0x5f)
+
+       def scan(self):
+           return [rom for rom in self.ow.scan() if rom[0] == 0x10 or rom[0] == 0x28]
+
+       def convert_temp(self):
+           self.ow.reset(True)
+           self.ow.writebyte(self.ow.SKIP_ROM)
+           self.ow.writebyte(_CONVERT)
+
+       def read_scratch(self, rom):
+           self.ow.reset(True)
+           self.ow.select_rom(rom)
+           self.ow.writebyte(_RD_SCRATCH)
+           self.ow.readinto(self.buf)
+           if self.ow.crc8(self.buf):
+               raise Exception('CRC error')
+           return self.buf
+
+       def write_scratch(self, rom, buf):
+           self.ow.reset(True)
+           self.ow.select_rom(rom)
+           self.ow.writebyte(_WR_SCRATCH)
+           self.ow.write(buf)
+
+       def read_temp(self, rom):
+           buf = self.read_scratch(rom)
+           if rom[0] == 0x10:
+               if buf[1]:
+                   t = buf[0] >> 1 | 0x80
+                   t = -((~t + 1) & 0xff)
+               else:
+                   t = buf[0] >> 1
+               return t - 0.25 + (buf[7] - buf[6]) / buf[7]
+           else:
+               t = buf[1] << 8 | buf[0]
+               if t & 0x8000:  # sign bit set
+                   t = -((t ^ 0xffff) + 1)
+               return t / 16
+
+    def init_read(self, ds):
+        ds.convert_temp()
+
+    def read (self, ds, rom):
+        ds.read_temp(rom)
